fb3b75fd9b46e51cefdfbea9d7f690a648f4de0c
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-09-30 23:22:58 +0200

    commit

--- src/main/index.php
+++ src/main/index.php
@@ -5,23 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UniChat</title>
+    <link rel="stylesheet" href="main_style.css">
 </head>
 
 <body>
-    
-<?php
+
+    <?php
     session_start();
 
-    if (isset($_SESSION["IsConnected"]) && $_SESSION["IsConnected"] == true)
-    {
-        ?>
-            <form action="" method="post">
-                <li class="nav-item">
-                    <button type='submit' name='Deconnexion' class="btn btn-primary">Se déconnecter</button>
-                </li>
-            </form>
-        <?php
-        
+    if (isset($_SESSION["IsConnected"]) && $_SESSION["IsConnected"] == true) {
+    ?>
+        <form action="" method="post">
+            <li class="nav-item">
+                <button type='submit' name='Deconnexion' class="btn btn-primary">Se déconnecter</button>
+            </li>
+        </form>
+    <?php
+
         if (isset($_POST["Deconnexion"])) // Sinon si l'utilisateur appuis sur le bouton de déconnexion
         {
             session_unset(); // On supprime tout les tableaux de la session
@@ -29,7 +29,24 @@
             header("Location: ../login-page/index.php");
         }
     }
-?>
+    ?>
+
+
+
+
+    <div class="container">
+        <div class="left">
+            Left
+        </div>
+        <div class="right">
+            Right
+        </div>
+    </div>
+
+
+
+
+
 </body>
 
 </html>
